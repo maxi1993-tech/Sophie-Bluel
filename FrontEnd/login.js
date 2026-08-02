3140338f5b24e9423901805de5bd6c18585a711d
maxi1993-tech <maxi1993.cm@gmail.com>
2026-08-02 14:26:00 +0200

    feat(login): read input values on form submit

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -5,7 +5,16 @@ function listenSubmit() {
     form.addEventListener("submit", (event) => {
         event.preventDefault()
         
+        const values = getInputValues()
+        console.log(values)
     })
 }
 listenSubmit()
 
+function getInputValues() {
+
+const inputEmail = document.querySelector("input[type='email']").value
+const inputPassword = document.querySelector("input[type='password']").value
+    
+    return {inputEmail, inputPassword}
+}
