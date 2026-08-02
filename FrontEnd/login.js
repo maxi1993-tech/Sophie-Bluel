6affd8a48c11083d39a09847f400edaaf8f5b588
maxi1993-tech <maxi1993.cm@gmail.com>
2026-08-02 14:20:31 +0200

    feat(login): listen to form submit and prevent default behavior

---
+++ FrontEnd/login.js
@@ -0,0 +1,11 @@
+function listenSubmit() {
+
+    const form = document.querySelector("form")
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        
+    })
+}
+listenSubmit()
+
