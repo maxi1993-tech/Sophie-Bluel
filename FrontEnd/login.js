e1cbee2c1a6e6b0e006a503599b4c4bb285238e9
maxi1993-tech <maxi1993.cm@gmail.com>
2026-08-06 09:58:36 +0200

    feat(login): listen to form submit

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,2 +1,10 @@
 import { fetchLogin } from './api.js'
 
+function listenSubmit() {
+
+    const loginForm = document.querySelector(".login-form")
+
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault()
+    })
+}
