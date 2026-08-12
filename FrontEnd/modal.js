17b80c9337372177151ea1b541db9fdcea6f4d4c
maxi1993-tech <maxi1993.cm@gmail.com>
2026-08-12 08:52:43 +0200

    feat(modal): add modal closing function

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,4 +1,11 @@
 function listenClick(button, fn) {
 
     button.addEventListener("click", fn)
-} 
+}
+
+function closeModal() {
+
+    const modalContainer = document.querySelector("#modal-container")
+    modalContainer.classList.remove("is-active")
+    modalContainer.ariaHidden = "true"
+}
